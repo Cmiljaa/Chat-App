d835a25f60c40e6361938be79adcff7f170498fb
Katušnik <147316652+Cmiljaa@users.noreply.github.com>
2025-06-07 23:23:39 +0200

    Firebase initializing

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
+import firebaseApp from './firebase'
 
 const app = createApp(App);
 
