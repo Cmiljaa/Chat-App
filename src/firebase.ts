4512dcfe683e676d1976466a8c3dff2a04702667
Katušnik <147316652+Cmiljaa@users.noreply.github.com>
2025-06-07 23:23:39 +0200

    Firebase initializing

---
+++ src/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCxKoo5GhPLoDynSwb_fK3ikr_Lpf_cTsg",
+  authDomain: "chat-app-33b9c.firebaseapp.com",
+  projectId: "chat-app-33b9c",
+  storageBucket: "chat-app-33b9c.firebasestorage.app",
+  messagingSenderId: "26125830552",
+  appId: "1:26125830552:web:9c8a96c4127153abfc03ed",
+  databaseURL: "https://chat-app-33b9c-default-rtdb.europe-west1.firebasedatabase.app"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+export default firebaseApp;
