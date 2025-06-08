8f867a5261b4f41cd07f0b3a8fd2c5b82af79662
Katušnik <147316652+Cmiljaa@users.noreply.github.com>
2025-06-08 23:47:33 +0200

    NavBar component added

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+	<div class="flex-col min-h-screen">
+		<NavBar></NavBar>
+
+		<RouterView></RouterView>
+	</div>
+
+</template>
+
+<script setup lang="ts">
+import NavBar from './components/NavBar.vue';
+
+</script>
