f4e1ec3163adb55acb268af52c9e830684d022bf
Katušnik <147316652+Cmiljaa@users.noreply.github.com>
2025-06-09 20:44:04 +0200

    Base input component added

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,48 @@
+<template>
+	<div class="relative mt-6">
+		<label :for="id" class="block mb-1 text-base text-black">
+			{{ label }}
+		</label>
+		<input :type="type" :id="id" :name="name" :value="modelValue" required @input="onInput"
+			class="w-full px-4 py-2 text-black placeholder-transparent bg-white border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500" />
+		<p class="text-red-500">
+			{{ error }}
+		</p>
+	</div>
+</template>
+
+<script setup lang="ts">
+
+import { defineProps, defineEmits } from 'vue';
+
+defineProps({
+	modelValue: String,
+	type: {
+		type: String,
+		default: 'text'
+	},
+	id: {
+		type: String,
+		required: true
+	},
+	name: {
+		type: String,
+		required: true
+	},
+	label: {
+		type: String,
+		required: true
+	},
+	error: {
+		required: false
+	},
+});
+
+const emit = defineEmits(['update:modelValue']);
+
+const onInput = (event: Event) => {
+	const target = event.target as HTMLInputElement;
+	emit('update:modelValue', target.value);
+}
+
+</script>
