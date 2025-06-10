11dea45302d7d189f4a0706822a190cc41a0a13e
Katušnik <147316652+Cmiljaa@users.noreply.github.com>
2025-06-10 18:59:36 +0200

    GoogleAuth

--- src/components/AuthForm.vue
+++ src/components/AuthForm.vue
@@ -1,16 +1,22 @@
 <template>
-	<form @submit.prevent="submitAuth" class="max-w-md mx-auto mt-14 p-6 bg-blue-50 rounded-lg shadow-md">
-		<h1 class="text-3xl font-medium text-black mb-8 text-center">{{ title }}</h1>
+	<form @submit.prevent="submitAuth" class="max-w-md mx-auto mt-10 p-6 bg-blue-50 rounded-lg shadow-md">
+		<h1 class="text-3xl font-medium text-black mb-2 text-center">{{ title }}</h1>
 
-		<div class="space-y-6">
-			<div class="space-y-6">
+		<div>
+			<div class="space-y-4">
 				<slot :v$="v$">
 
 				</slot>
 				<button type="submit" :disabled="loading"
-					class="w-full py-2 bg-blue-600 text-white text-lg font-semibold rounded-md hover:bg-blue-700 hover:cursor-pointer transition disabled:opacity-50 disabled:cursor-not-allowed">
+					class="w-full py-2 bg-blue-600 text-white text-base font-semibold rounded-md hover:bg-blue-700 hover:cursor-pointer transition disabled:opacity-50 disabled:cursor-not-allowed">
 					Submit
 				</button>
+				<a @click="googleAuth(router)"
+					class="flex items-center justify-center w-full py-2 px-4 bg-white text-black border border-gray-300 rounded-md hover:cursor-pointer hover:bg-gray-100 transition duration-150 ease-in-out">
+					<img src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/google.svg" alt="Google logo"
+						class="w-5 h-5 mr-3" />
+					Continue with Google
+				</a>
 			</div>
 		</div>
 	</form>
@@ -26,6 +32,7 @@
 import { ref } from 'vue';
 import { useRouter } from 'vue-router';
 import { useVuelidate } from '@vuelidate/core';
+import { googleAuth } from '../firebase/GoogleAuth';
 
 const props = defineProps<{
 	formData: Record<string, any>,
---
+++ src/firebase/GoogleAuth.ts
@@ -0,0 +1,15 @@
+import { type Router } from "vue-router";
+import { signInWithPopup, GoogleAuthProvider, getAuth } from "firebase/auth";
+
+const provider = new GoogleAuthProvider();
+
+export const googleAuth = async (router: Router) => {
+	try {
+		const result = await signInWithPopup(getAuth(), provider)
+		GoogleAuthProvider.credentialFromResult(result);
+		await router.push('/messages');
+
+	} catch (error) {
+		console.log(error);
+	}
+}
