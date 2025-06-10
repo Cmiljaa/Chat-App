8b70de773b7a3e6649a8b51c07aa0f8f34b541bb
Katušnik <147316652+Cmiljaa@users.noreply.github.com>
2025-06-10 18:59:36 +0200

    GoogleAuth

--- src/components/BaseInput.vue
+++ src/components/BaseInput.vue
@@ -1,11 +1,11 @@
 <template>
-	<div class="relative mt-6">
-		<label :for="id" class="block mb-1 text-base text-black">
+	<div class="relative mt-2">
+		<label :for="id" class="block mb-0.5 text-sm text-black">
 			{{ label }}
 		</label>
 		<input :type="type" :id="id" :name="name" :value="modelValue" required @input="onInput"
-			class="w-full px-4 py-2 text-black placeholder-transparent bg-white border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500" />
-		<p class="text-red-500">
+			class="w-full px-3 py-1.5 text-lg text-black placeholder-transparent bg-white border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500" />
+		<p class="text-red-500 text-base mt-1">
 			{{ error }}
 		</p>
 	</div>
