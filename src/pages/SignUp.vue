9c41ea5840da9f45f9a27f9d3d94e060527acd3d
Katušnik <147316652+Cmiljaa@users.noreply.github.com>
2025-06-10 19:02:48 +0200

    Removed logic from SignIn and SignUp files, improved error handling

--- src/pages/SignUp.vue
+++ src/pages/SignUp.vue
@@ -20,15 +20,11 @@
 </template>
 
 <script setup lang="ts">
-import { ref, reactive } from 'vue';
-import { useRouter } from 'vue-router';
+import { computed, reactive } from 'vue';
 import { registerNewUser } from '../firebase/SignUp';
-import useVuelidate from '@vuelidate/core';
 import { required, minLength, email, sameAs, helpers } from '@vuelidate/validators';
-
-const loading = ref<boolean>(false);
-const router = useRouter();
-let authError = ref<string | null>(null);
+import AuthForm from '../components/AuthForm.vue';
+import BaseInput from '../components/BaseInput.vue';
 
 const formData = reactive({
 	nickname: '',
@@ -41,34 +37,19 @@ const rules = {
 	nickname: { required, minLength: minLength(5) },
 	email: { required, email },
 	password: { required, minLength: minLength(6) },
-	repeatPassword: { required, sameAsPassword: helpers.withMessage('Passwords do not match', sameAs(() => formData.password)) }
+	repeatPassword: { required, sameAsPassword: helpers.withMessage('Passwords do not match', sameAs(computed(() => formData.password))) }
 }
 
-const v$ = useVuelidate(rules, formData);
-const registerUser = async () => {
-	try {
-		await createUserWithEmailAndPassword(auth, email.value, password.value);
-
-		await writeUserData(nickname.value, email.value);
-
-		const user = await registerNewUser(formData.nickname, formData.email, formData.password);
-		console.log(user);
-		await router.push('/messages');
-
-	} catch (error: any) {
-		switch (error.code) {
-			case 'auth/weak-password':
-				authError.value = 'Password should be at least 6 characters long.';
-				break;
-			case 'auth/email-already-in-use':
-				authError.value = 'This email is already registered. Please sign in or use another email.';
-				break;
-			case 'auth/invalid-email':
-				authError.value = 'Please enter a valid email address.';
-				break;
-			default:
-				authError.value = 'An unexpected error occurred. Please try again.';
-		}
+const handleErrors = (error: any) => {
+	switch (error.code) {
+		case 'auth/weak-password':
+			return 'Password should be at least 6 characters long.';
+		case 'auth/email-already-in-use':
+			return 'This email is already registered. Please sign in or use another email.';
+		case 'auth/invalid-email':
+			return 'Please enter a valid email address.'
+		default:
+			return 'An unexpected error occurred. Please try again.';
 	}
 }
 
