3b5f41245b36627259057a9161dcbfad92f66cb6
Katušnik <147316652+Cmiljaa@users.noreply.github.com>
2025-06-12 22:04:13 +0200

    User store added

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import './style.css'
 import App from './App.vue'
 import router from './router'
-import './firebase/firebase';
+import './firebase/firebase'
 
 const app = createApp(App);
 
 app.use(router);
+
+app.use(createPinia());
 
 app.mount('#app')
