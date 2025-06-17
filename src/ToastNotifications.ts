f0075e82dd91ff2bb7d886d7c599a4fbaa80eb90
Katušnik <147316652+Cmiljaa@users.noreply.github.com>
2025-06-17 15:58:04 +0200

    Add toast notifications

---
+++ src/ToastNotifications.ts
@@ -0,0 +1,15 @@
+import { useToast } from 'vue-toast-notification'
+import 'vue-toast-notification/dist/theme-sugar.css'
+
+const $toast = useToast()
+
+const showToast = (method: 'success' | 'info' | 'warning' | 'error', message: string): void => {
+	
+  const instance = $toast[method](message, { duration: 5000 })
+
+  setTimeout(() => {
+    instance.dismiss();
+  }, 3000)
+};
+
+export default showToast;
--- src/firebase/SignUp.ts
+++ src/firebase/SignUp.ts
@@ -1,5 +1,6 @@
 import { getDatabase, ref as dbRef, set } from "firebase/database";
 import { getAuth, createUserWithEmailAndPassword, type UserCredential } from "firebase/auth";
+import showToast from "../ToastNotifications";
 
 export const registerNewUser = async (nickname: string, email: string, password: string): Promise<UserCredential> => {
 	const user = await createUserWithEmailAndPassword(getAuth(), email, password);
@@ -10,6 +11,8 @@ export const registerNewUser = async (nickname: string, email: string, password:
 		nickname,
 		email,
 	});
+
+	showToast('success', 'Your account was successfully created.');
 
 	return user;
 }
