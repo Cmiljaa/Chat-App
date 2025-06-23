b9c032dffe68b45c09625dcc6287e90173716135
Katušnik <147316652+Cmiljaa@users.noreply.github.com>
2025-06-23 21:54:36 +0200

    Add Modal component

---
+++ src/components/UI/Modal.vue
@@ -0,0 +1,25 @@
+<template>
+	<div v-if="isOpen" class="fixed inset-0 z-50 flex items-center justify-center bg-black/50 backdrop-blur-sm">
+		<div class="bg-[#1f1f1f] text-white w-full max-w-md mx-4 p-6 rounded-2xl shadow-xl relative">
+			<button
+				class="absolute top-2 right-2 sm:top-3 sm:right-3 text-gray-400 hover:text-white text-2xl sm:text-3xl leading-none focus:outline-none hover:cursor-pointer"
+				@click="closeModal">
+				&times;
+			</button>
+
+			<slot></slot>
+		</div>
+	</div>
+</template>
+
+
+<script setup lang="ts">
+import { defineProps, defineEmits } from 'vue';
+
+defineProps<{ isOpen: boolean }>();
+const emit = defineEmits(['update:isOpen']);
+
+const closeModal = () => {
+	emit('update:isOpen', false);
+};
+</script>
