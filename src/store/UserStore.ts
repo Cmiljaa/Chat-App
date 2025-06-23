d3e5b64283c2b7e6c3beb9e0f8a334b6248c30e5
Katušnik <147316652+Cmiljaa@users.noreply.github.com>
2025-06-23 21:55:11 +0200

    Refactor UserStore to update user type and actions

--- src/store/UserStore.ts
+++ src/store/UserStore.ts
@@ -1,21 +1,19 @@
-import type { User } from "firebase/auth";
 import { defineStore } from "pinia";
+import type User from "../interfaces/user";
 
 export const useUserStore = defineStore('userStore', {
 	state: () => ({
 		user: null as null | User
 	}),
-	getters: {
-		getUser(): User | null{
-			return this.user;
-		}
-	},
-	actions: ({
-		setUser(user: User){
-			this.user = user;
+	actions: {
+		setUser(user: User | null){
+			if(user)
+				this.user = user;
+			else
+				return;
 		},
 		removeUser(){
 			this.user = null;
 		}
-	})
+	}
 });
