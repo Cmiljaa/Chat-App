75e33b37f6352c68380b39a79f9502590f2adbe8
Katušnik <147316652+Cmiljaa@users.noreply.github.com>
2025-07-02 16:58:03 +0200

    Refactor cursor-pointer

--- src/components/UI/Modal.vue
+++ src/components/UI/Modal.vue
@@ -2,7 +2,7 @@
 	<div v-if="isOpen" class="fixed inset-0 z-50 flex items-center justify-center bg-black/50 backdrop-blur-sm">
 		<div class="bg-[#1f1f1f] text-white w-full max-w-md mx-4 p-6 rounded-2xl shadow-xl relative">
 			<button
-				class="absolute top-2 right-2 sm:top-3 sm:right-3 text-gray-400 hover:text-white text-2xl sm:text-3xl leading-none focus:outline-none hover:cursor-pointer"
+				class="absolute top-2 right-2 sm:top-3 sm:right-3 text-gray-400 hover:text-white text-2xl sm:text-3xl leading-none focus:outline-none cursor-pointer"
 				@click="closeModal">
 				&times;
 			</button>
