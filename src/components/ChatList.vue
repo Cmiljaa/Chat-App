ee863f06374cc9bd4c25c6be6f4912616b964cdd
Katušnik <147316652+Cmiljaa@users.noreply.github.com>
2025-07-06 10:11:58 +0200

    Refactor chat item click to use openChat method

--- src/components/ChatList.vue
+++ src/components/ChatList.vue
@@ -1,6 +1,6 @@
 <template>
 	<div>
-		<p v-if="chats.length" v-for="chat in chats" :key="chat.id" @click="loadChat(otherUser.id, otherUser.nickname)"
+		<p v-if="chats.length" v-for="chat in chats" :key="chat.id" @click="openChat(chat.id)"
 			class="text-white text-lg px-6 py-3 hover:bg-white/10 transition-colors duration-150 cursor-pointer truncate">
 			{{ getOtherMemberNickname(chat.members, userId) }}
 		</p>
@@ -11,19 +11,22 @@
 </template>
 
 <script setup lang="ts">
+import { useRouter } from 'vue-router';
 import type { Chat } from '../interfaces/chat';
 
 defineProps<{
 	chats: Chat[],
 	userId: string
 }>();
-
+const router = useRouter();
 
 const getOtherMemberNickname = (members: Record<string, { id: string, nickname: string }>, excludeId: string): string => {
 	const other = Object.values(members).find(member => member.id !== excludeId);
 	return other ? other.nickname : 'Unknown';
 }
 
-
+const openChat = (chatId: string) => {
+	router.push({ name: 'Chat', params: { chatId: chatId } });
+};
 
 </script>
