29f5c659a3427c44a19502214f135ef317176234
Katušnik <147316652+Cmiljaa@users.noreply.github.com>
2025-07-06 10:10:24 +0200

    Refactor user service from RTDB to Firestore

--- src/firebase/services/userService.ts
+++ src/firebase/services/userService.ts
@@ -1,36 +1,31 @@
-import { ref, get, getDatabase, child, query, orderByChild, startAt, endAt, limitToFirst } from "firebase/database";
+import { getFirestore, serverTimestamp, doc, setDoc, query, getDoc, collection, getDocs, orderBy, startAt, endAt } from "firebase/firestore";
+import showToast from "../../ToastNotifications";
+import type { User } from "../../interfaces/user";
 
-export const getUsers = async (exceptUserId?: string) => {
-	const db = getDatabase();
-	const usersRef = ref(db, 'users');
+export const addUserToDb = async (nickname: string, email: string, id: string): Promise<void> => {
+	const db = getFirestore();
+
 	try {
-		const snapshot = await get(usersRef);
-		if (snapshot.exists()) {
-			let users = snapshot.val();
-
-			if(exceptUserId){
-				users = Object.fromEntries(
-				Object.entries(users).filter(([userId]) => userId !== exceptUserId));
-			}
-
-			return users;
-		} else {
-			console.log("No users found.");
-			return null;
-		}
+		await setDoc(doc(db, 'users', id), {
+			nickname,
+			email,
+			id,
+			created_at: serverTimestamp()
+		}, { merge: true });
 	} catch (error) {
-		console.log(error);
+		console.error('Failed to add user to DB:', error);
+		showToast('error', 'Unexpected error');
 	}
-};
+}
 
-export const getUser = async (userId: string): Promise<null> => {
-	const db = getDatabase();
-	const userRef = ref(db);
+export const getUser = async (userId: string): Promise<User | null> => {
+	const db = getFirestore();
+	const userRef = doc(db, 'users', userId);
 	try {
-		const snapshot = await get(child(userRef, `users/${userId}`));
+		const snapshot = await getDoc(userRef);
 		if(snapshot.exists()){
-			return snapshot.val();
-		}else{
+			return snapshot.data() as User;
+		} else{
 			console.log('No data available');
 			return null;
 		}
@@ -40,29 +35,32 @@ export const getUser = async (userId: string): Promise<null> => {
 	}
 }
 
-export const getUsersByNickname = async (nickname: string) => {
-	const db = getDatabase();
-	const usersRef = ref(db, 'users');
+export const getUsersByNickname = async (nickname: string): Promise<User[]> => {
+
+const db = getFirestore();
+const usersRef = collection(db, 'users');
 
 const userQuery = query(
-  	usersRef,
-  	orderByChild('nickname'),
-  	startAt(nickname),
-  	endAt(nickname + "\uf8ff"),
-  	limitToFirst(10)
-);
+		usersRef,
+		orderBy('nickname'),
+		startAt(nickname),
+		endAt(nickname + '\uf8ff')
+	);
 
 try {
-	const snapshot = await get(userQuery);
-	 if (snapshot.exists()) {
-      console.log(snapshot.val());
-      return snapshot.val();
+	const snapshot = await getDocs(userQuery);
+	if (!snapshot.empty) {
+		const users = snapshot.docs.map(doc => ({
+			id: doc.id,
+			...doc.data()
+		}));
+
+		return users as User[];
     } else {
-      console.log("No users");
-      return null;
+		console.log("No users found");
+		return [];
     }
 } catch (error) {
 	console.error(error);
-}
-
-};
+	return [];
+}};
