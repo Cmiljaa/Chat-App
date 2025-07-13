0c3776a6256970b0bdbac31a98ad716042ba2ec8
Katušnik <147316652+Cmiljaa@users.noreply.github.com>
2025-07-13 11:56:48 +0200

    Pass nickname as query param when opening chat

--- src/composables/chat/useChatList.ts
+++ src/composables/chat/useChatList.ts
@@ -2,9 +2,9 @@ import type { Router } from "vue-router";
 
 export default function useChatList(){
 
-	const openChat = (chatId: string, router: Router): void => {
-		router.push({ name: 'Chat', params: { chatId: chatId } });
-	};
+	const openChat = (chatId: string, router: Router, nickname: string): void => {
+		router.push({ name: 'Chat', params: { chatId: chatId }, query: { nickname: nickname } });
+	}; 
 
 	const getOtherMemberNickname = (members: Record<string, { id: string, nickname: string }>, excludeId: string): string => {
 		const other = Object.values(members).find(member => member.id !== excludeId);
