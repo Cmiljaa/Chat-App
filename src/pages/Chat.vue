c2cf8e02cb5e8ee22f6f3147caa504afd01d0dff
Katušnik <147316652+Cmiljaa@users.noreply.github.com>
2025-07-15 13:11:11 +0200

    Add auto-scroll to bottom in chat on message update

--- src/pages/Chat.vue
+++ src/pages/Chat.vue
@@ -5,7 +5,7 @@
 				class="bg-[#0d0d0d] px-6 py-7 h-12 flex items-center border-l border-gray-700 sticky top-0 z-10 w-full shadow-sm">
 				<span class="text-xl font-semibold text-white">{{ otherUserNickname ?? 'Unknown' }}</span>
 			</div>
-			<div class="flex-1 overflow-y-auto p-4 space-y-2 flex flex-col">
+			<div ref="chatContainer" class="flex-1 overflow-y-auto p-4 space-y-2 flex flex-col">
 				<div v-for="chatMessage in chatMessages" :key="chatMessage.id" class="flex"
 					:class="chatMessage.senderId === user.id ? 'justify-end' : 'justify-start'">
 					<div :class="chatMessage.senderId === user.id
@@ -24,6 +24,7 @@
 						:class="['px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition cursor-pointer', { 'bg-gray-400 ': isDisabled }]">
 						Send
 					</button>
+					<button @click="scrollToBottom">Test Scroll</button>
 				</div>
 			</div>
 		</div>
@@ -32,7 +33,7 @@
 </template>
 
 <script setup lang="ts">
-import { type ComputedRef } from 'vue';
+import { nextTick, watchEffect, type ComputedRef } from 'vue';
 import useCurrentUser from '../composables/auth/useCurrentUser';
 import type { User } from '../interfaces/user';
 import { useRoute } from 'vue-router';
@@ -42,6 +43,16 @@ import useChatMessages from '../composables/chat/useChatMessages';
 const route = useRoute();
 const { user }: { user: ComputedRef<User> } = useCurrentUser();
 
-const { isLoading, chatMessages, message, handleSendingMessage, isDisabled, otherUserNickname } = useChatMessages(user, route);
+const { chatContainer, scrollToBottom, isLoading, chatMessages, message, handleSendingMessage, isDisabled, otherUserNickname } = useChatMessages(user, route);
+
+watchEffect(() => {
+	if (!isLoading) {
+		nextTick(() => {
+			scrollToBottom();
+		});
+	}
+});
+
+
 
 </script>
