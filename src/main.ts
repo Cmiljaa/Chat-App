6346aa402880f1584c1e2ee51a7a1ed7f9048f14
Katušnik <147316652+Cmiljaa@users.noreply.github.com>
2025-07-17 17:31:37 +0200

    Add vue3-chat-scroll for auto-scrolling chat messages
    https://www.npmjs.com/package/vue3-chat-scroll

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,15 @@ import router from './router'
 import './firebase/firebaseInit';
 import ToastPlugin from 'vue-toast-notification';
 
+import VueChatScroll from 'vue3-chat-scroll';
+
 const app = createApp(App);
 
 app.use(router);
 
 app.use(createPinia());
+
+app.use(VueChatScroll);
 
 app.use(ToastPlugin);
 
---
+++ src/vue3-chat-scroll.d.ts
@@ -0,0 +1,5 @@
+declare module 'vue3-chat-scroll' {
+  import { Plugin } from 'vue';
+  const Vue3ChatScroll: Plugin;
+  export default Vue3ChatScroll;
+}
