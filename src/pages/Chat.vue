b458aeab4f8be665d4ce78d0264b7cfc5cf06184
Katušnik <147316652+Cmiljaa@users.noreply.github.com>
2025-07-20 10:24:28 +0200

    Improve chat message UI with action menu and styling

--- src/pages/Chat.vue
+++ src/pages/Chat.vue
@@ -6,13 +6,31 @@
 				<span class="text-xl font-semibold text-white">{{ otherUserNickname ?? 'Unknown' }}</span>
 			</div>
 			<div ref="chatContainer" class="flex-1 overflow-y-auto p-4 space-y-2 flex flex-col" v-chat-scroll>
-				<div v-for="chatMessage in chatMessages" :key="chatMessage.id" class="flex"
+				<div v-for="chatMessage in chatMessages" :key="chatMessage.id" class="flex group"
 					:class="chatMessage.senderId === user.id ? 'justify-end' : 'justify-start'">
-					<div :class="chatMessage.senderId === user.id
-						? 'bg-blue-500 text-white rounded-bl-lg rounded-tr-lg rounded-tl-lg shadow-md'
-						: 'bg-gray-200 text-gray-900 rounded-br-lg rounded-tl-lg rounded-tr-lg shadow-sm'" class="p-2 w-fit max-w-xs">
-						{{ chatMessage.text }}
-					</div>
+					<template v-if="chatMessage.senderId === user.id">
+						<div class="relative flex items-start">
+							<span class="p-2 text-base cursor-pointer opacity-0 group-hover:opacity-100 relative"
+								@click="">⋯</span>
+							<div class="flex justify-end">
+								<div
+									class="p-2 max-w-2xl break-words bg-blue-500 rounded-bl-lg rounded-tr-lg rounded-tl-lg text-white">
+									{{ chatMessage.text }}
+								</div>
+							</div>
+						</div>
+					</template>
+
+					<template v-else>
+						<div class="relative flex items-start">
+							<span
+								class="p-2 max-w-2xl break-words bg-gray-200 text-gray-900 rounded-br-lg rounded-tr-lg rounded-tl-lg shadow-sm">
+								{{ chatMessage.text }}
+							</span>
+							<span class="p-2 text-base cursor-pointer opacity-0 group-hover:opacity-100">⋯</span>
+						</div>
+					</template>
+
 				</div>
 			</div>
 
@@ -25,6 +43,7 @@
 		</textarea>
 					<button @click="handleSendingMessage" :disabled="isDisabled"
 						:class="['px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition cursor-pointer', { 'bg-gray-400 ': isDisabled }]">
+						:class="['px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 text-lg transition cursor-pointer', { 'bg-gray-400 ': isDisabled }]">
 						Send
 					</button>
 				</div>
