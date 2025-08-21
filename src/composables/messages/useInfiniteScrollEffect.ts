acbb024f011bdc5161bf2923b25577a9cc788ecc
Katušnik <147316652+Cmiljaa@users.noreply.github.com>
2025-08-21 15:28:41 +0200

    Add useInfiniteScrollEffect composable for infinite scrolling

---
+++ src/composables/messages/useInfiniteScrollEffect.ts
@@ -0,0 +1,40 @@
+import { nextTick, watch, type Ref } from "vue";
+import { useInfiniteScroll } from '@vueuse/core';
+import { getChatMessages } from "../../firebase/services/messageService";
+import type { Message } from "../../interfaces/message";
+
+export default function useInfiniteScrollEffect(chatId: Ref<string>, chatMessages: Ref<Message[]>, listEl: Ref<HTMLDivElement | null>, loadMore: Ref<boolean>){
+
+	watch(listEl, (el) => {
+		if (el && loadMore.value && listEl.value) {
+
+			useInfiniteScroll(
+				el,
+				async () => {
+					
+					if (el.scrollHeight <= el.clientHeight || !listEl.value) return;
+
+					const prevHeight = listEl.value.scrollHeight;
+
+					const result = await getChatMessages(chatId.value, chatMessages, true);
+
+					await nextTick();
+
+					const newHeight = listEl.value.scrollHeight;
+
+					listEl.value.scrollTop += (newHeight - prevHeight);
+
+
+					if (!result || result === 0) {
+						loadMore.value = false;
+						return;
+					}
+				},
+				{
+					distance: 0,
+					direction: 'top',
+				}
+			);
+		}
+	});
+}
