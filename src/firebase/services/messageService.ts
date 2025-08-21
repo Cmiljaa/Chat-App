9702885f4ecb52148e021a2976d9b599610d32d9
Katušnik <147316652+Cmiljaa@users.noreply.github.com>
2025-08-21 15:29:02 +0200

    Add pagination to getChatMessages in messageService

--- src/firebase/services/messageService.ts
+++ src/firebase/services/messageService.ts
@@ -1,9 +1,10 @@
-import { collection, deleteDoc, doc, getDoc, getFirestore, onSnapshot, orderBy, query, serverTimestamp, setDoc, where } from "firebase/firestore";
+import { collection, deleteDoc, doc, getDoc, getFirestore, limit, onSnapshot, orderBy, query, serverTimestamp, setDoc, startAfter, where } from "firebase/firestore";
 import showToast from "../../ToastNotifications";
 import type { Message } from "../../interfaces/message";
-import type { Ref } from "vue";
+import { ref, type Ref } from "vue";
 
 let unsubscribeMessages: (() => void) | null = null;
+let lastVisibleDoc: any = ref(null);
 
 export const sendMessage = async (senderId: string, messageText: string, chatId: string): Promise<string | null> => {
 	const db = getFirestore();
@@ -25,51 +26,86 @@ export const sendMessage = async (senderId: string, messageText: string, chatId:
 	}
 }
 
-export const getChatMessages = async (chatId: string, messages: Ref<Message[]>): Promise<void> => {
+export const getChatMessages = async (chatId: string, messages: Ref<Message[]>, loadMore: boolean = false): Promise<number | null> => {
 
 	const db = getFirestore();
 	const messagesRef = collection(db, 'messages');
 
-	const messagesQuery = query(messagesRef, where('chatId', '==', chatId), orderBy('createdAt', 'asc'));
+	const numberOfMessages: Ref<number | null> = ref(null);
+
+	let messagesQuery;
+	if (loadMore && lastVisibleDoc) {
+		messagesQuery = query(
+		messagesRef,
+		where("chatId", "==", chatId),
+		orderBy("createdAt", "desc"),
+		startAfter(lastVisibleDoc),
+		limit(30));
+	} else {
+		messagesQuery = query(
+		messagesRef,
+		where("chatId", "==", chatId),
+		orderBy("createdAt", "desc"),
+		limit(30));
+	}
 
 	try {
 
-		if (unsubscribeMessages) {
+		if (unsubscribeMessages && !loadMore) {
 			unsubscribeMessages();
 		}
 
-		unsubscribeMessages = onSnapshot(messagesQuery, (querySnapshot) => {
-			querySnapshot.docChanges().forEach((change) => {
-				const doc = change.doc
-				const data = doc.data() as Message
-
-				const message: Message = { ...data }
-
-				switch (change.type) {
-					case 'added':
-							messages.value.splice(change.newIndex, 0, message)
-						break;
-
-					case 'modified':
-							if (change.oldIndex !== change.newIndex) {
-								messages.value.splice(change.oldIndex, 1);
-								messages.value.splice(change.newIndex, 0, message);
-							} else {
-								messages.value.splice(change.newIndex, 1, message);
-							}
-						break;
-
-					case 'removed':
-							messages.value.splice(change.oldIndex, 1)
-						break;
-				
-					default:
-						break;
+		const firstSnapshot = new Promise<number>((resolve, reject) => {
+			unsubscribeMessages = onSnapshot(messagesQuery, (querySnapshot) => {
+				if (!querySnapshot.empty) {
+					lastVisibleDoc = querySnapshot.docs[querySnapshot.docs.length - 1];
 				}
-			})
+
+				numberOfMessages.value = querySnapshot.docs.length;
+
+				querySnapshot.docChanges().forEach((change) => {
+					const doc = change.doc
+					const data = doc.data() as Message
+
+					const message: Message = { ...data }
+
+					switch (change.type) {
+						case 'added':
+								if (loadMore) {
+									messages.value = [...messages.value, message];
+								} else {
+									const newMessages = [...messages.value];
+									newMessages.splice(change.newIndex, 0, message);
+									messages.value = newMessages;
+								}
+							break;
+
+						case 'modified':
+								if (change.oldIndex !== change.newIndex) {
+									messages.value.splice(change.oldIndex, 1);
+									messages.value.splice(change.newIndex, 0, message);
+								} else {
+									messages.value.splice(change.newIndex, 1, message);
+								}
+							break;
+
+						case 'removed':
+								messages.value.splice(change.oldIndex, 1)
+							break;
+					
+						default:
+							break;
+					}
+				});
+					resolve(querySnapshot.docs.length);
+				}, (error) => {
+					reject(error);
+				});
 		});
+		return await firstSnapshot;
 	} catch (error) {
 		console.error(error);
+		return null;
 	}
 };
 
